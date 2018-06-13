43e64a65cb0f8e65efef2566e13fe16738710c10
Fiona Xie <fifix2001@gmail.com>
2018-06-13 11:24:05 -0400

    fix population solution

--- PopulationCount/src/Population.java
+++ PopulationCount/src/Population.java
@@ -1,6 +1,15 @@
+import java.util.Scanner;
+
 public class Population {
 
-    public int getPopulationCount(int binaryNum){
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter binary number: ");
+        int num = reader.nextInt();
+        System.out.println(getPopulationCount(num));
+    }
+
+    public static int getPopulationCount(int binaryNum){
         int count = 0;
         while (binaryNum > 0){
             if (binaryNum % 10 == 1){
