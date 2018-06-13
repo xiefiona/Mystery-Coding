60ea7d5c2c0ed4b406f2e7b432609901154b02aa
Fiona Xie <fifix2001@gmail.com>
2018-06-13 11:31:28 -0400

    change name

--- VariableNotation/src/Notation.java
+++ VariableNotation/src/Notation.java
@@ -1,7 +1,7 @@
 import java.util.ArrayList;
 import java.util.Scanner;
 
-public class Tester {
+public class Notation {
     public static void main(String args[]){
         Scanner reader = new Scanner(System.in);
 
