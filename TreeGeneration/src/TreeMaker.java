7f7da75bbb496420ed7012734b65b7007f229679
Fiona Xie <fifix2001@gmail.com>
2018-06-13 11:39:05 -0400

    fix tree generation solution

--- TreeGeneration/src/TreeMaker.java
+++ TreeGeneration/src/TreeMaker.java
@@ -1,6 +1,6 @@
 import java.util.*;
 
-public class Tester {
+public class TreeMaker {
     public static void main(String args[]){
         Scanner reader = new Scanner(System.in);
         System.out.println("Enter number between 3 and 21: ");
@@ -11,14 +11,22 @@ public class Tester {
         String tree = reader.next();
 
         String string;
+        int space = num/2;
         for (int i=1; i<=num; i+=2){
             string = "";
-            for (int x=0; x<=i; x++){
+            for (int x=0; x<space; x++){
+                string += " ";
+            }
+            for (int x=0; x<i; x++){
                 string += tree;
             }
             System.out.println(string);
+            space--;
         }
         string = "";
+        for (int i=0; i<(num/2)-1; i++){
+            string+= " ";
+        }
         for (int i=0; i<3; i++){
             string+=trunk;
         }
