2fc5207b0e28726c33e7f144fa48339c96f5cad0
Fiona Xie <fifix2001@gmail.com>
2018-06-13 12:05:28 -0400

    fix subset sum solution

--- SubsetSum/src/SubsetSum.java
+++ SubsetSum/src/SubsetSum.java
@@ -1,6 +1,21 @@
+import java.util.Scanner;
+
 public class SubsetSum {
 
-    public boolean isSum(int[] input){
+    public static void main(String[] args){
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter list: ");
+        String[] arrSplit = reader.nextLine().split(" ");
+        int[] arrNums = new int[arrSplit.length];
+        for (int i=0; i<arrSplit.length; i++){
+            arrNums[i] = Integer.parseInt(arrSplit[i]);
+        }
+
+        System.out.println(isSum(arrNums));
+
+    }
+
+    public static boolean isSum(int[] input){
         for(int i=0; i<input.length; i++){
             if(input[i]==0){
                 return true;
