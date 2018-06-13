825e407391abcdc798519abdfb10c55cdcb55130
Fiona Xie <fifix2001@gmail.com>
2018-06-13 12:00:03 -0400

    fix letter by letter solution

--- LetterByLetter/src/LetterChange.java
+++ LetterByLetter/src/LetterChange.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Tester {
+public class LetterChange {
     public static void main(String[] args){
         Scanner reader = new Scanner(System.in);
         System.out.println("input 1: ");
@@ -8,6 +8,8 @@ public class Tester {
         System.out.println("input 2: ");
         String string2 = reader.nextLine();
 
+        System.out.println();
+        System.out.println(string1);
         for (int i=0; i<string1.length(); i++){
             if (!string1.substring(i,i+1).equals(string2.substring(i,i+1))){
                 System.out.println(string2.substring(0,i+1) + string1.substring(i+1));
