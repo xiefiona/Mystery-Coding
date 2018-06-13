d17a6260fb12c979cb475f13735888e2e8a84aca
Fiona Xie <fifix2001@gmail.com>
2018-06-13 11:30:35 -0400

    fix variable notation solution

--- VariableNotation/src/Tester.java
+++ VariableNotation/src/Tester.java
@@ -1,22 +1,68 @@
+import java.util.ArrayList;
 import java.util.Scanner;
 
 public class Tester {
     public static void main(String args[]){
         Scanner reader = new Scanner(System.in);
-        VariableNotation function = new VariableNotation();
 
         System.out.println("Enter number: ");
         int input = reader.nextInt();
 
         System.out.println("Enter string: ");
-        String string = reader.next();
+        reader.nextLine();
+        String string = reader.nextLine();
 
         if (input == 0){
-            System.out.println(function.getCamcelCase(string));
+            System.out.println(getCamcelCase(string));
         } else if (input ==  1){
-            System.out.println(function.getSnakeCase(string));
+            System.out.println(getSnakeCase(string));
         } else if (input == 2){
-            System.out.println(function.getCapSnakeCase(string));
+            System.out.println(getCapSnakeCase(string));
         }
+    }
+
+    public static String getCamcelCase(String input){
+        ArrayList<String> arrLet = new ArrayList<>();
+        for (int i=0; i < input.length(); i++){
+            arrLet.add(input.substring(i,i+1));
+        }
+
+        for (int i=0; i < arrLet.size(); i++){
+            if (arrLet.get(i).equals(" ")){
+                arrLet.remove(i);
+                arrLet.set(i,arrLet.get(i).toUpperCase());
+            }
+        }
+
+        String string = "";
+        for (int i=0; i < arrLet.size(); i++){
+            string += arrLet.get(i);
+        }
+
+        return string;
+    }
+
+    public static String getSnakeCase(String input){
+        ArrayList<String> arrLet = new ArrayList<>();
+        for (int i=0; i < input.length(); i++){
+            arrLet.add(input.substring(i,i+1));
+        }
+
+        for (int i=0; i < arrLet.size(); i++){
+            if (arrLet.get(i).equals(" ")){
+                arrLet.set(i,"_");
+            }
+        }
+
+        String string = "";
+        for (int i=0; i < arrLet.size(); i++){
+            string += arrLet.get(i);
+        }
+
+        return string;
+    }
+
+    public static String getCapSnakeCase(String input){
+        return (getSnakeCase(input)).toUpperCase();
     }
 }
